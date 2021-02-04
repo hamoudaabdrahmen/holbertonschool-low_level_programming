83434f2b522557c5f6441380b6b824046ce6151d
hamoudaabdrahmen <abdrahmenbenhamouda@gmail.com>
2021-02-04 20:50:18 +0100

    my tasks

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    for(int x = 'A'; x <= 'Z'; x++) // jumps from 'A'(65) to 'B'(66) to ... in order.
+    {
+        putchar(tolower(x)); // the return value is returned as a parameter
+    }
+    return 0;
+}

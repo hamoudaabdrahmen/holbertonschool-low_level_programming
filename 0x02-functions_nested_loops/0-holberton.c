cd760357be45960840ae302ba613631fa07b7a3f
hamoudaabdrahmen <abdrahmenbenhamouda@gmail.com>
2021-02-09 10:46:31 +0100

    correction

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -6,12 +6,12 @@
  */
 int main()
 {
-  int i;
-  char t[] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n', '\n'};
+int i;
+char t[] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n', '\n'};
 
-  for (i = 0; i < 10; i++)
-    {
-      _putchar(t[i]);
-    }
-    return (0);
+for (i = 0; i < 10; i++)
+{
+_putchar(t[i]);
+}
+return (0);
 }

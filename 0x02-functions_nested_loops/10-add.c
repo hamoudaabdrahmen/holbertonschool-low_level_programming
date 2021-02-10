4e2a2da85c11ca430de6a143b1ff82b5bd06eb36
hamoudaabdrahmen <abdrahmenbenhamouda@gmail.com>
2021-02-10 17:28:01 +0100

    add two int

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
  * main - check the code for Holberton School students.
@@ -8,9 +7,9 @@
  */
 int main(void)
 {
-    int n;
+int n;
 
-    n = add(89, 9);
-    printf("%d\n", n);
-    return (0);
+n = add(89, 9);
+printf("%d\n", n);
+return (0);
 }

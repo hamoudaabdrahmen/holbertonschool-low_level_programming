6030327a26dbaec664d0ebe70e301fa08423dbf8
hamoudaabdrahmen <abdrahmenbenhamouda@gmail.com>
2021-02-16 18:51:36 +0100

    update

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,14 +1,15 @@
 #include "holberton.h"
 /**
- * int _strlen - prints the lengh of a string
+ * _strlen - calculates the lengh of a string.
  * @s: the string to be dispayed
+ * Return: the lengh of string.
  */
 int _strlen(char *s)
 {
 int i;
 int count = 0;
 
-for(i = 0; s[i] != '\0';i++)
+for (i = 0; s[i] != '\0'; i++)
 {
 count++;
 }

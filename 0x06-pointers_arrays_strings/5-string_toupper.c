1a2459e769d79db80e0c30565f1648864c0e7fe4
hamoudaabdrahmen <abdrahmenbenhamouda@gmail.com>
2021-02-18 21:40:15 +0100

    tasks

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * *string_toupper - converts lowercase to uppercase
+ * @s:given string.
+ */
+char *string_toupper(char *s)
+{
+    int i = 0;
+
+    for (; s[i] != '\0'; i++)
+    {
+        if (s[i] > 96 && s[i] < 123)
+        {
+            s[i] -= 32;
+        }
+        
+    }
+    return(s);
+}

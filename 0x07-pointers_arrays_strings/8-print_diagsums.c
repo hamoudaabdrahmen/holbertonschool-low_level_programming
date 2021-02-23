1c85954bef69254523807b7a2d3e56a9bff68c49
hamoudaabdrahmen <abdrahmenbenhamouda@gmail.com>
2021-02-23 14:14:46 +0100

    sum of two diags

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * _diagsums - prints the sum of the two diagonals of a square matrix of integers.
+ * print_diagsums - prints the sum of the two diagonals.
  * @a: matrix
  * @size: size of matrix
  */
@@ -14,7 +14,7 @@ for (i = 0; i < sizem; i += size + 1)
 sum += a[i];
 printf("%li, ", sum);
 sum = 0;
-for ( i = size - 1; i <= sizem - size + 1; i += size - 1)
+for (i = size - 1; i <= sizem - size + 1; i += size - 1)
 sum += a[i];
 printf("%li\n", sum);
 }

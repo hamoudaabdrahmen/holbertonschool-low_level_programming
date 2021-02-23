8f69be7d9b0f29a41b6d789891729fa4bd1da9aa
hamoudaabdrahmen <abdrahmenbenhamouda@gmail.com>
2021-02-23 14:11:46 +0100

    update

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -7,27 +7,17 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-int i, j, pos, flag = 0;
-for (i = 0; s[i] != '\0'; i++)
-pos = i;
-for (i = 0; accept[i] != '\0'; i++)
-{
-for (j = 0; s[j] != '\0'; j++)
-{
-if (accept[i] == s[j])
-if (j <= pos)
-pos = j;
-flag = 1;
-}
-}
-}
-}
-if (flag == 1)
-{
-return (&s[pos]);
-}
-else
-{
-return (0);
-}
+	unsigned int i, j;
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+				break;
+		}
+		if (accept[j])
+			return (s + i);
+	}
+	return (0);
 }

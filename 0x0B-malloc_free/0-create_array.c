8ac5d5fca155758cf740ef8c83c2805dc77f51a7
hamoudaabdrahmen <abdrahmenbenhamouda@gmail.com>
2021-03-02 22:31:18 +0100

    malloc_free project tasks

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,14 +12,14 @@ unsigned int i;
 char *a;
 if (size == 0)
 return (NULL);
-a = malloc (size * sizeof(char));
+a = malloc(size * sizeof(char));
 if (a == NULL)
 {
 return (NULL);
 }
-for ( i = 0; i < size; i++)
+for (i = 0; i < size; i++)
 {
 a[i] = c;
 }
-return(a);
+return (a);
 }
